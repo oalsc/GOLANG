a97d117273085e63f53e553535d0db9e8f076d63
oalsc <andersonluissalazar@outlook.com>
2021-02-03 15:40:24 -0300

    operadores

---
+++ 5 - Operadores/operadores.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Aritmeticos
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisao := 10 / 4
+	multiplicacao := 10 * 5
+	restoDaDivisao := 10 % 2
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDaDivisao)
+
+	var numero1 int16 = 10
+	var numero2 int16 = 25
+	soma2 := numero1 + numero2
+	fmt.Println(soma2)
+
+	// Atribuição
+	fmt.Println("-----------")
+	var variavel1 string = "String"
+	variavel2 := "String2"
+
+	fmt.Println(variavel1, variavel2)
+
+	// Relacionais
+	fmt.Println("-----------")
+	fmt.Println(1 > 2)
+	fmt.Println(1 >= 2)
+	fmt.Println(1 == 2)
+	fmt.Println(1 <= 2)
+	fmt.Println(1 < 2)
+	fmt.Println(1 != 2)
+
+	// Logicos
+	fmt.Println("-----------")
+	verdadeiro, falso := true, false
+	fmt.Println(verdadeiro && falso)
+	fmt.Println(verdadeiro || falso)
+	fmt.Println(!falso)
+
+	// Unarios
+	fmt.Println("-----------")
+	numero := 10
+	numero++
+	numero += 15 // numero = numero + 15
+	fmt.Println(numero)
+
+	numero--
+	numero -= 20 // numero = numero - 20
+	fmt.Println(numero)
+
+	numero *= 3 // numero = numero * 3
+	numero /= 10
+	numero %= 3
+	fmt.Println(numero)
+}
