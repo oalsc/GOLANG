7c90717e6477acd3de9ec5f930fc6ea78184271f
oalsc <andersonluissalazar@outlook.com>
2021-02-03 22:05:29 -0300

    Arrays internos

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -28,4 +28,22 @@ func main() {
 
 	array2[1] = "Posicao alterada"
 	fmt.Println(slice2)
+
+	// Arrays internos
+	fmt.Println("----------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 12)
+	slice3 = append(slice3, 16)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // Length
+	fmt.Println(cap(slice3)) // Capacity
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 12.25)
+	fmt.Println(len(slice3)) // Length
+	fmt.Println(cap(slice3)) // Capacity
 }
