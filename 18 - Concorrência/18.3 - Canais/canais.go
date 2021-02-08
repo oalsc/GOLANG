e43a9dfaebda2ae0852fe1edf47d48e136f23d47
oalsc <andersonluissalazar@outlook.com>
2021-02-08 17:56:57 -0300

    Concorrência 1/2

---
+++ 18 - Concorrência/18.3 - Canais/canais.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal := make(chan string)
+	go escrever("ola mundo", canal)
+
+	fmt.Println("depois da funcao escrever")
+
+	for {
+		mensagem, aberto := <-canal
+		if !aberto {
+			break
+		}
+		fmt.Println(mensagem)
+	}
+
+	fmt.Println("Fim do programa")
+}
+
+func escrever(texto string, canal chan string) {
+	for i := 0; i < 5; i++ {
+		canal <- texto
+		time.Sleep(time.Second)
+	}
+	close(canal)
+}
