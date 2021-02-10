a3d6fd42906a9274742872dee4e5616d7bd6b1ff
oalsc <andersonluissalazar@outlook.com>
2021-02-10 01:55:46 -0300

    Testes avançados

---
+++ 19 - Testes Automatizados/19.2 - Testes Avançados/formas/formas_test.go
@@ -0,0 +1,28 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Retângulo", func(t *testing.T) {
+		ret := Retangulo{10, 20}
+		areaEsperada := float64(200)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+
+	t.Run("Circulo", func(t *testing.T) {
+		circ := Circulo{10}
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := circ.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+}
